d534ff2d20786eb713fe0113833f4dbedc1bb9a8
Logan Norman <logannormanss@gmail.com>
2022-07-04 07:55:25 -0700

    add collapsable folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
 	return <div>
 		<Folder name="Desktop">
+			<Folder name="Music">
+				<File name="wokeuplikethis.mp3" />
+				<File name="startedfromthebottom.mp3" />
+				<File name="draco.mp3" />
+			</Folder>
 			<File name="cheese.js" />
 			<File name="queso.js" />
 			<File name="butter.js" />
@@ -13,11 +18,20 @@ function App() {
 }
 
 const Folder = (props) => {
-	console.log(props);
-	const borderStyle = { border: '2px solid green' }
-	return <div style={borderStyle}>
-		{props.name}
-		{props.children}
+	const [isOpen, setIsOpen] = useState(false);
+	const {name, children} = props;
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	}
+	
+	const childMargin = { marginLeft: '20px' };
+	return <div>
+		<span onClick={handleClick}>
+			{name}
+		</span>
+		<div style={childMargin}>
+			{isOpen ? children : null}
+		</div>
 	</div>
 }
 
