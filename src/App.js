b01848fbcad3b1b92fb839bca02300457455218a
Logan Norman <logannormanss@gmail.com>
2022-07-04 07:33:01 -0700

    rendering components w/ style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function App() {
-	return 'Hello. This is my new app!';
+	return <div>
+		<Folder name="Desktop">
+			<File name="cheese.js" />
+			<File name="queso.js" />
+			<File name="butter.js" />
+		</Folder>
+		<Folder name="Applications"/>
+	</div>
+
+}
+
+const Folder = (props) => {
+	console.log(props);
+	const borderStyle = { border: '2px solid green' }
+	return <div style={borderStyle}>
+		{props.name}
+		{props.children}
+	</div>
+}
+
+const File = (props) => {
+	return <div>{props.name}</div>
 }
 
 export default App;
